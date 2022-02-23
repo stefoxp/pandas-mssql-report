03a608b585e2a2b066f449aba6bdca4b1d520e67
stefoxp <stefano.pasquini.2010@gmail.com>
2022-02-23 09:09:09 +0100

    Fail msg pytest

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ def main():
     )
     cn = cls_cn.connection()
 
-    s_str = 'SELECT TOP 10 * FROM COMU_MATRICOLE WHERE Annullato = 0;'
+    # s_str = 'SELECT TOP 10 * FROM COMU_MATRICOLE WHERE Annullato = 0;'
+    s_str = '''SELECT TOP 100 
+    ct.Codice as TipoUtenteCodice, ct.Descrizione As TipoUtente, ct.Presidio As PresidioPerTipoUtente
+    , cm.Matricola As CodiceEnte, cm.CodiceFiscale, cm.Cognome, cm.Nome, cm.Sesso, cm.Email, cm.Cellulare 
+    FROM COMU_MATRICOLE cm
+        INNER JOIN COMU_TIPOLOGIE ct
+            ON ct.Codice = cm.Tipologia 
+    WHERE Annullato = 0'''
 
     cls_pd = DbWithPandas(cn)
     df = cls_pd.query(s_str)
--- test/test_config.py
+++ test/test_config.py
@@ -4,6 +4,6 @@ import os.path
 
 class TestConfigSecrets:
     def test_secrets_exist_is_true(self):
-        path_to_secrets = 'config/secrets.py'
+        path_to_secrets = 'cofig/secrets.py'
 
-        assert os.path.exists(path_to_secrets) == True
+        assert os.path.exists(path_to_secrets) == True, "Il File " + path_to_secrets + " non e' presente"
